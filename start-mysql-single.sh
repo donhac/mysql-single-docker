038fd9bf3e198793c61b442512a5017582a75eee
donh <donhac@qq.com>
2018-06-06 10:08:49 +0800

    init

---
+++ start-mysql-single.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p $PWD/data
+mkdir -p $PWD/logs
+chmod 644 $PWD/conf/my.cnf
+
+# 强制杀死正在运行的 mysql-single
+docker rm -f mysql-single
+
+# mysql single
+docker run -d --restart=on-failure:5 --name mysql-single \
+              -p 3306:3306 \
+              -e "MYSQL_ROOT_PASSWORD=my-mysql" \
+              -v $PWD/conf:/etc/mysql/conf.d \
+              -v $PWD/data:/var/lib/mysql \
+              -v $PWD/logs:/var/log \
+              mysql:5.7.12 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
